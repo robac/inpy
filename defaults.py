54be3a2b8bfa71ad2ad35a350a3b123d1cfd376c
robac <robert@poch.cz>
2017-04-21 12:58:09 +0200

    init commit

---
+++ defaults.py
@@ -0,0 +1,14 @@
+DEFAULT_ARGUMENTS = {
+   'config-file' : 
+	{
+		'value' : '/etc/inpy/inpy.conf',
+		'description' : 'path to config file',
+		'argument' : True
+	},
+   'log-file' : 
+	{
+		'value' : '/var/log/inpy.log',
+		'description' : 'path to log file',
+		'argument' : False
+	}
+}
